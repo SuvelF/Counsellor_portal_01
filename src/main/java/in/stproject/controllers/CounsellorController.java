2688c027c50a7f89b3dec57af6307b308dde17a0
Suhel <tadsaresuel@gmail.com>
2026-02-23 00:32:34 +0530

    implemented controller class with adding login and register methods

---
+++ src/main/java/in/stproject/controllers/CounsellorController.java
@@ -0,0 +1,96 @@
+package in.stproject.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import in.stproject.dto.CounsellorDTO;
+import in.stproject.dto.DashboardResponseDTO;
+import in.stproject.service.CounsellorService;
+import in.stproject.service.EnquiryService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class CounsellorController {
+
+	@Autowired
+	private CounsellorService counsellorService;
+	
+	
+	@Autowired
+	private EnquiryService enqService;
+	
+	@GetMapping("/")
+	public String index(Model model) {
+		
+	    CounsellorDTO cdto = new CounsellorDTO();
+	    model.addAttribute("counsellor",cdto);
+	    
+		return "index";
+	}
+	
+	
+	@PostMapping("/login")
+	public String handleLogin(HttpServletRequest req, CounsellorDTO dto , Model model) {
+		CounsellorDTO counsellor = counsellorService.login(dto);
+		
+		if(counsellor == null) {
+			model.addAttribute("emsg","Invalid Credentials");
+			CounsellorDTO cdto = new CounsellorDTO();
+		    model.addAttribute("counsellor",cdto);
+			return "index";
+		}
+		else {
+			Integer counsellorId = counsellor.getCounsellorId();
+			
+			HttpSession session = req.getSession();
+			session.setAttribute("counsellorId", counsellorId);
+			
+			DashboardResponseDTO dashboardDto = enqService.getDashboardInfo(counsellorId);
+			
+			model.addAttribute("dashboardDto", dashboardDto);
+		
+			return "dashboard";
+		}
+		
+		
+	}
+	
+	
+	@GetMapping("/register")
+	public String registerPage(Model model) {
+		
+	    CounsellorDTO cdto = new CounsellorDTO();
+	    model.addAttribute("counsellor",cdto);
+	    
+		return "register";
+	}
+	
+	
+	@PostMapping("/register")
+	public String handlingRegister(@ModelAttribute("counsellor") CounsellorDTO counsellor, Model model) {
+		
+		boolean unique = counsellorService.uniqueEmailCheck(counsellor.getEmail());
+		
+		if(unique) {
+			boolean register = counsellorService.register(counsellor);
+			if(register) {
+			model.addAttribute("smsg","Registration Success");
+		}
+			else {
+				model.addAttribute("emsg","Registration Failed");
+			}
+		
+	}
+		else {
+			model.addAttribute("emsg","Enter unique Email");
+		}
+	
+       return "register";
+	}
+	
+}
--- src/main/java/in/stproject/service/EnquiryServiceImpl.java
+++ src/main/java/in/stproject/service/EnquiryServiceImpl.java
@@ -1,11 +1,13 @@
 package in.stproject.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
 import in.stproject.dto.DashboardResponseDTO;
@@ -84,18 +86,66 @@ public class EnquiryServiceImpl implements EnquiryService{
 	@Override
 	public List<EnquiryDTO> getEnquiries(Integer counsellorId) {
 		// TODO Auto-generated method stub
-		return null;
+		List<EnquiryDTO> enqsDtoList = new ArrayList<>();
+		
+		List<EnquiryEntity> enqList = enqRepo.findByCounsellorCounsellorId(counsellorId);
+		for(EnquiryEntity entity : enqList) {
+			EnquiryDTO dto = new EnquiryDTO();
+			BeanUtils.copyProperties(entity, dto);
+			enqsDtoList.add(dto);
+		}
+		return enqsDtoList;
 	}
 
 	@Override
 	public List<EnquiryDTO> getEnquiries(EnqFilterDTO filterDTO, Integer counsellorId) {
 		// TODO Auto-generated method stub
-		return null;
+	
+		EnquiryEntity entity = new EnquiryEntity();
+		if(filterDTO.getClassMode()!=null && !filterDTO.getClassMode().equals("")) {
+			entity.setClassMode(filterDTO.getClassMode());
+		}
+		if(filterDTO.getCourse()!=null && !filterDTO.getCourse().equals("")) {
+			entity.setCourse(filterDTO.getCourse());
+		}
+		if(filterDTO.getEneStatus()!=null && !filterDTO.getEneStatus().equals("")) {
+			entity.setEneStatus(filterDTO.getEneStatus());
+		}
+		
+		
+		CounsellorEntity counsellor = new CounsellorEntity();
+		counsellor.setCounsellorId(counsellorId);
+		entity.setCounsellor(counsellor);
+		
+		Example<EnquiryEntity> of = Example.of(entity);
+		
+		List<EnquiryEntity> enqList = enqRepo.findAll(of);
+		
+		List<EnquiryDTO> enqsDtoList = new ArrayList<>();
+		
+		for(EnquiryEntity enq : enqList) {
+			EnquiryDTO dto = new EnquiryDTO();
+			BeanUtils.copyProperties(entity, dto);
+			enqsDtoList.add(dto);
+		}
+		
+		return enqsDtoList;
+	
 	}
 
 	@Override
 	public EnquiryDTO getEnquiryById(Integer enqId) {
 		// TODO Auto-generated method stub
+		
+		
+		Optional<EnquiryEntity> byId =enqRepo.findById(enqId);
+		
+		if(byId.isPresent()) {
+			EnquiryEntity enquiryEntity = byId.get();
+			EnquiryDTO dto = new EnquiryDTO();
+			BeanUtils.copyProperties(enquiryEntity, dto);
+			return dto;
+		}
 		return null;
 	}
 
